a7be1c7af78e707db270f8d827fb29c93f028774
Zach Juinio <zachjuinio@yahoo.com.ph>
2018-09-12 17:28:56 +1000

    Updated to include defined variables in config.php

--- SIT302/changePassword.php
+++ SIT302/changePassword.php
@@ -1,11 +1,13 @@
 <?php
+include "config.php";
     $username=$_POST["username"];
     $password=htmlentities($_POST["password"]);
 	$password=md5('salt1024'.$password);
-	$conn = new mysqli('localhost','root','','scrappertest');
+	$conn = new mysqli(DB_HOST,DB_USER,DB_PWD,DB_TABLENAME);
     $sql="update users set password='{$password}' where username = '{$username}'";
     $conn->query($sql);
 	
 	
     
     echo 1;
+?>
