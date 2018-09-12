14d54e586412cc619d2169be5ab214e48ab5172e
Zach Juinio <zachjuinio@yahoo.com.ph>
2018-09-12 15:14:52 +1000

    Updated the href for bootstrap

--- SIT302/price_analysis.php
+++ SIT302/price_analysis.php
@@ -8,9 +8,8 @@
   <meta charset="UTF-8" />
   <title>Global Obesity Program</title>
   <link rel="stylesheet" type="text/css" href="css/style.css" />
-  <link rel="stylesheet" href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css">  
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  
   <script src="http://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
-  <script src="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <link rel="stylesheet" href="asset.css">
   <style type="text/css">
 
