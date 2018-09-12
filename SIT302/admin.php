96526ccebd3c6b7bdd32b2b4c5b43c0d98e1d7f9
Zach Juinio <zachjuinio@yahoo.com.ph>
2018-09-12 15:14:23 +1000

    Updated the href for bootstrap

--- SIT302/admin.php
+++ SIT302/admin.php
@@ -12,9 +12,8 @@
 <html>
 <head>
 	<title></title>
-    <link rel="stylesheet" href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css">  
+    <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">  
 	<script src="http://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
-	<script src="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="asset.css">
 	<style type="text/css">
 		.nav{
