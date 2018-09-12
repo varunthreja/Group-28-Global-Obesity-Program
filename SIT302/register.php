e1ba89f773f8753dc53139c8ca074fc66c1443cc
garbs95 <jgarbell@deakin.edu.au>
2018-09-12 12:33:54 +1000

    Adding design consistency

--- SIT302/register.php
+++ SIT302/register.php
@@ -16,6 +16,8 @@
 	<link rel="stylesheet" href="http://cdn.static.runoob.com/libs/bootstrap/3.3.7/css/bootstrap.min.css">  
 	<script src="http://cdn.static.runoob.com/libs/jquery/2.1.1/jquery.min.js"></script>
 	<link rel="stylesheet" href="asset.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css" />
+
 <style>
 
 .error{color:red;}
@@ -27,7 +29,7 @@
   display:flex;
   align-items:center;
   justify-content:center;
-  position:absolute;
+  //position:absolute;
   top:0;
   left:0;
   right:0;
@@ -51,12 +53,52 @@
 
 <body>
 <nav class="navbar navbar-default navbar-fixed-top" role="navigation">
-    <div class="container-fluid"> 
-    <div class="navbar-header">
-        <a class="navbar-brand" href="index.php" style="color:white">Healthy Diets ASAP</a>
-    </div>
-</nav>	
+		<div class="container-fluid"> 
+			<div class="navbar-header">
+				<a class="navbar-brand" href="index.php" style="color:white">Healthy Diets ASAP</a>
+				<a class="navbar-brand" href="product_information_input.php" style="color:white">| Product Detail Input</a>
+			</div>
 
+			<div id="user" >
+				<?php  
+
+
+					if (isset($_COOKIE["username"])){
+						echo  '<ul class="nav nav-pills navbar-nav navbar-right"> <li><a href="account_setting.php" style="color:white"><span ></span>'.$_COOKIE["username"].'</a></li><li><a href="logout.php" style="color:white">Log out</a></li></ul>';
+					}
+					else{
+						# echo  '<script>var c=confirm("We plan to use cookie to provide you a better shopping evironment,do you want to start cookie?");if(c==true){alert("cookie start")}else{alert("cookie banned")}</script>';
+						echo  '<ul class="nav nav-pills navbar-nav navbar-right"> <li><a href="register.php" style="color:white"><span class="glyphicon glyphicon-user"></span>  Register</a></li><li><a href="login.php" style="color:white"><span class="glyphicon glyphicon-log-in"> Log in</a></li></ul>';
+					}   
+				?>
+			</div>
+
+			<form class="navbar-form navbar-right" role="search">
+				<div class="form-group">
+					<input type="text" class="form-control" placeholder="Search">
+				</div>
+				<button type="submit" class="btn btn-default">submit</button>
+			</form>
+
+		</div>
+	</nav>
+    
+	<div id="page">
+		<div id="header">
+			<div>
+				<a href="index.php"><img src="images/GlobalObesityLogo.png" alt="Logo" /></a>
+			</div>
+			<ul>
+				<li class="current"><a href="index.php"><span>Home</span></a></li>
+				<?php  
+					if(isset($_COOKIE["username"])){
+						echo '<li><a href="account_setting.php"><span>My Account</span></a></li><li><a href="product_detail.php"><span>Product input</span></a></li><li><a href="price_analysis.php"><span>Price Analysis</span></a></li><li><a href="product_information.php"><span>Products</span></a></li>';
+					} 
+				?>
+				<li><a href="about.php"><span>About Us</span></a></li>
+			</ul>
+		</div>
+	</div>
 
    <div class="registerTable">
   
