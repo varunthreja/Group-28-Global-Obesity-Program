3566e8aae4428c706c4ede29ad462b551af6ee06
DevilFaker <625018684@qq.com>
2018-09-13 14:11:52 +1000

    add update1
    account_setting require

--- SIT302/doAction.php
+++ SIT302/doAction.php
@@ -16,6 +16,10 @@ if($act==="reg"){
 	update();
 }else if($act==="input"){
     input();
+}else if($act==="update1"){
+    update();
+}else if($act==="search"){
+    search();
 }
 
 function update(){
@@ -30,19 +34,28 @@ function update(){
         }
 }
 
-// function freeze(){
-        // $connect=oci_connect(DB_USER,DB_PWD,DB_HOST) ;
-		// $userID=$_POST["userID"];
-        // $sql="update users set confirmed='N' where userID='{$userID}' ";
-        // $stmt=oci_parse($connect,$sql);
-        // $result=oci_execute($stmt);
+function update1(){
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PWD, DB_TABLENAME);
+    //apply  htmlentities function
+    $username=$_COOKIE["username"];
+    $organisation=htmlentities($_POST['organisation']);
+    $organsiationAddress=htmlentities($_POST['organsiationAddress']);
+    $position=htmlentities($_POST['position']);
+    $email=htmlentities($_POST['email']);
+    $contactNumber=htmlentities($_POST['contactNumber']);
+    $name=htmlentities($_POST['realname']);
+
+    $result="update users set name='{$name}',organisation='{$organisation}',organsiationAddress='{$organsiationAddress}',position='{$position}',email='{$email}',contactNumber='{$contactNumber}' where username='{$username}';";
+    # $conn->exec($result);
+    if (mysqli_query($conn, $result)) {
+        
+        echo "<script>window.location='account_setting.php'</script>";
+
+    } else {
+        echo "Error: " . $result . "<br>" . mysqli_error($conn);
+    }
        
-        // if($result){
-            // echo 1;
-        // }else{
-            // echo 2;
-        // }
-// }
+}
 
 function reg(){
     $conn = new mysqli(DB_HOST, DB_USER, DB_PWD, DB_TABLENAME);
@@ -147,14 +160,19 @@ function input(){
     $yourCost=htmlentities($_POST['yourCost']);
     $comments=htmlentities($_POST['comments']);
     $pricePromote=htmlentities($_POST['pricePromote']);
- 
-    
-    
-    //$email=htmlentities($_POST['email']);
    echo $foodName;
     
+}
 
-    
+function search(){
+    $foodName=htmlentities($_POST['foodName']);
+    $conn=new mysqli(DB_HOST, DB_USER, DB_PWD, DB_TABLENAME);
+    $sql="select * from fooddetails where foodName ='{$foodName}'";
+    $stmt=$conn->query($sql);
+    $results=mysqli_fetch_array($stmt);
+    if($results){
+        echo row["foodSpecificBrand"].'+'.row["servingSize"];
+    }
 }
 
 ?>
