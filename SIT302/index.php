ddca38869011daa6573e024a0b8457feaa909f87
DevilFaker <625018684@qq.com>
2018-09-19 15:30:28 +1000

    product detail input header remove

--- SIT302/index.php
+++ SIT302/index.php
@@ -18,7 +18,6 @@
 		<div class="container-fluid"> 
 			<div class="navbar-header">
 				<a class="navbar-brand" href="index.php" style="color:white">Healthy Diets ASAP</a>
-				<a class="navbar-brand" href="product_information_input.php" style="color:white">| Product Detail Input</a>
 			</div>
 
 			<div id="user" >
@@ -35,14 +34,6 @@
 				?>
 			</div>
 
-			<form class="navbar-form navbar-right" role="search">
-				<div class="form-group">
-					<input id="search" type="text" class="form-control" placeholder="Search">
-				   <div id="display"></div>
-
-				</div>
-				<button type="submit" class="btn btn-default">submit</button>
-			</form>
 
 		</div>
 	</nav>
