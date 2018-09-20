79c8271e1f3fc775b872a9f7a2f1c0ae5b4006d0
DevilFaker <625018684@qq.com>
2018-09-20 15:25:41 +1000

    price_analysis v1.0
    still have some errors

--- SIT302/price_analysis.php
+++ SIT302/price_analysis.php
@@ -131,23 +131,35 @@
     </ul>
             
     </div>
-   </div>     
+   </div> 
+<div class="search_form">    
+ <div class="navbar-form search_box" role="search">
+        <div class="form-group">
+            <input type="text" id="search_input" name="name" class="form-control search_input" placeholder="Search">
+        </div>
+        
+        
+</div>    
+</div>
+
 <div class="main" id="showBox" >
 
-<div class="showBox table-responsive" >
+<div class="showBox table-responsive"  >
 
 
 <table class="table table-bordered table-hover">
 
     <tbody id="table1">
 		<?php 
+      $foodName=array();
 			$sql="select * from foodDetails ORDER BY foodName";
 			$conn=new mysqli(DB_HOST, DB_USER, DB_PWD, DB_TABLENAME);
 			$results=$conn->query($sql);
       $i=0;
 			while($row=$results->fetch_assoc()){
-
+        array_push($foodName, $row["foodName"]);
         $i++;
+
 			if($i%5==1){
 			  echo '<tr>';
 			}
@@ -157,7 +169,7 @@
 			  echo '<td><input type="checkbox" name="product" value="'.$row["foodName"].'">'.$row["foodName"].'</td>';
 			}
 			else if($row["foodName"][0]=='"'){
-			  echo '<td><input type="checkbox" name="product" value='.$row["foodName"].'>'.$row["foodName"].'</td>';
+			  echo '<td><input type="checkbox" name="product" value='.$row["foodName"].' >'.$row["foodName"].'</td>';
 			}
 
 			if($i%5==0){
@@ -235,6 +247,7 @@
   var button=document.getElementById("product_submit");
   var startDate;
   var endDate;
+  var originalSelectValue=[];
   $("#start").change(function(){
     $("#start").attr("value",$(this).val()); //赋值
     startDate=$("#start").val();
@@ -253,7 +266,7 @@
 
   button.addEventListener("click",function(){
   var selectProduct = '';
-$('input:checkbox[name="product"]:checked').each(function(k){
+  $('input:checkbox[name="product"]:checked').each(function(k){
     if(k == 0){
         selectProduct = $(this).val();
     }else{
@@ -262,12 +275,63 @@ $('input:checkbox[name="product"]:checked').each(function(k){
 })
 
   var sql="SELECT a.collectionDate, a.price, b.foodName FROM main AS a, foodDetails AS b WHERE a.foodID  = (SELECT foodID FROM foodDetails WHERE foodName = '"+selectProduct+"') AND a.foodID = b.foodID AND a.collectionDate BETWEEN '"+startDate+"' AND '"+endDate+"'";
-
   alert(sql);
+  });
 
 
 
-  })
+   var foodName = <?php echo json_encode($foodName) ?>;//php array => json 
+  $("#search_input").on("keyup",function(){
+   var search_input=$("#search_input").val().toString();
+   var result=filterArray(foodName,search_input);
+   document.getElementById("table1").innerHTML="";
+   var updateTable;
+   for(let i=0;i<result.length;i++){
+      if((i+1)%5==1){
+        updateTable+='<tr>';
+      }
+      updateTable+='<td><input type="checkbox" name="product" value=';
+      updateTable+=result[i];
+      if(originalSelectValue.indexOf(result[i])>-1){
+      updateTable+=' checked="checked">';
+      }else{
+      updateTable+=' >';  
+      }
+      updateTable+=result[i];
+      updateTable+='</td>';
+      if((i+1)%5==0){
+       updateTable+="</tr>";
+      }
+   }
+
+  document.getElementById("table1").innerHTML=updateTable;
+  document.getElementById("pagination").innerHTML="";
+  });
+
+ function filterArray(array,clue){
+    var resultArray=[];
+    for(let i=0;i<array.length;i++){
+        var testString=array[i].toLowerCase();
+        if(testString.indexOf(clue)>-1){
+          resultArray.push(array[i]);
+        } 
+    }
+    return resultArray;
+  }
+
+  $("#table1").on("click",function(e){
+      var e=e||window.event;
+      var target=e.target;
+    $('input:checkbox[name="product"]:checked').each(function(k){
+        if(originalSelectValue.indexOf($(this).val())==-1){
+          originalSelectValue.push($(this).val());
+        }
+    });
+      alert(originalSelectValue);
+      //originalSelectValue.push(selectValue);
+  });
+
+
 
 </script>
 
